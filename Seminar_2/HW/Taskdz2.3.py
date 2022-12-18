638a39bdf9c9e3ea6d43262ed5e6752c9d61d038
BatmanovSergeydotnet <xoolly@mail.ru>
2022-12-19 01:34:23 +0300

    add Taskdz 2.5

--- Seminar_2/HW/Taskdz2.3.py
+++ Seminar_2/HW/Taskdz2.3.py
@@ -9,8 +9,8 @@
 n = int(input("Введите число: "))
 summ = 0
 num_list = []
-for n in range(1, n + 1):
-    num_list.append(round(((1 + 1/n) ** n), 3))
+for i in range(1, n + 1):
+    num_list.append(round(((1 + 1/i) ** i), 3))
 print(num_list)
 
 for i in range(0, n):
--- Seminar_2/HW/Taskdz2.4.py
+++ Seminar_2/HW/Taskdz2.4.py
@@ -8,5 +8,29 @@
 # -> [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 # -> 20
 
-n = int(input("Введите число: "))
+# Enter the value of N: 4
+# Position one: 20
+# Position two: 22
+# -> [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+# -> There are no values for these indexes!
 
+import os
+def clear(): return os.system('cls')
+clear()
+
+n = int(input("Введите число: "))
+num_list = []
+for i in range(-n, n+1):
+    num_list.append(i)
+print(num_list)
+print("Введите позиции элементов.")
+pos1 = int(input("Первая позиция: "))
+pos2 = int(input("Вторая позиция: "))
+composition = 1
+if pos1 <= len(num_list) and pos2 <= len(num_list):
+    composition = num_list[pos1-1] * num_list[pos2-1]
+    print(num_list)
+    print(composition)
+else:
+    print('В данном списке нет элементов с такими позициями')
+    print(num_list)
