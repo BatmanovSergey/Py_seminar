5dbdc96d91067d05cc8cbe91c7e80c9885970818
BatmanovSergeydotnet <xoolly@mail.ru>
2022-12-19 21:55:36 +0300

    корректировка Taskdz2.5

--- Seminar_2/HW/Taskdz2.1.py
+++ Seminar_2/HW/Taskdz2.1.py
@@ -6,13 +6,25 @@
 # - 0.67 -> 13
 # - 198.45 -> 27
 
+# # Решение 1
+
 n = float(input("Введите число: "))
 while n != int(n):
     n = n * 10
-print(n)
 summ = 0
 
 while n > 0:
     summ = summ + n % 10
     n = n // 10
 print(int(summ))
+
+# # Решение 2
+# n = input("Введите число: ")
+# m = len(n)
+# nf = float(n)
+# nf = nf * (10 ** (m-2))
+# summ = 0
+# while nf > 0:
+#     summ = summ + nf % 10
+#     nf = nf // 10
+# print(int(summ))
--- Seminar_2/HW/Taskdz2.5.py
+++ Seminar_2/HW/Taskdz2.5.py
@@ -8,9 +8,37 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
-n = int(input("Введите число: "))
-num_list = []
-for i in range(n):
-    num_list.append(i)
+# # Решение 1
+from random import randrange
+
+num_list = list(range(int(input("Введите число: "))))
 print(num_list)
+
+for i in range(len(num_list)):
+    index = randrange(len(num_list))
+    temp = num_list[i]
+    num_list[i] = num_list[index]
+    num_list[index] = temp
+print(num_list)
+
+# # Решение 2
+
+# import random
+
+# n = int(input("Введите число: ")) # вариант задания списка через append
+# num_list = []
+# for i in range(n):
+#     num_list.append(i)
+# print(num_list)
+
+# for i in range(n):
+#     index = random.randrange(n)
+#     temp = num_list[i]
+#     num_list[i] = num_list[index]
+#     num_list[index] = temp
+# print(num_list)
+
+# n = int(input("Введите число: ")) # вариант задания списка через list(range(n))
+# num_list = list(range(n))
+# print(num_list)
 
