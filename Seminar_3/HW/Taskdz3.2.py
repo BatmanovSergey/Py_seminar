1c0360ba9404c32781a2c30ae1bcce9dfddc41d4
BatmanovSergeydotnet <xoolly@mail.ru>
2022-12-21 20:22:02 +0300

    add Taskdz3.2

---
+++ Seminar_3/HW/Taskdz3.2.py
@@ -0,0 +1,36 @@
+# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Примеры:
+# in
+# 4
+# out
+# [2, 5, 8, 10]
+# [20, 40]
+
+# in
+# 5
+# out
+# [2, 2, 4, 8, 8]
+# [16, 16, 4]
+
+from random import sample
+import os
+def clear(): return os.system('cls')
+clear()
+
+def find_number(amount):
+    new_list = sample(range(1, (amount+1)*2), k=amount)
+    return new_list
+
+def multiplication_elements(new_list):
+    mulpli_list = []
+    if len(new_list) % 2 == 0:
+        for i in range(len(new_list)//2):
+            mulpli_list.append = new_list[i]
+    return mulpli_list
+
+num = int(input('Введите количество чисел: '))
+num_list = find_number(num)
+print(num_list)
+m_list = multiplication_elements(num_list)
+print(m_list)
