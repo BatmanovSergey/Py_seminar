e37092d3246ce581f9bfe21b8478ddeedb9187c5
BatmanovSergeydotnet <xoolly@mail.ru>
2023-01-14 19:05:18 +0300

    add Taskdz5.1.

--- Seminar_5/Task5.2.py
+++ Seminar_5/Task5.2.py
@@ -10,17 +10,18 @@ def any_list(num):
 def seq(some_list):
     temp_list = []
     for i in range(len(some_list)):
-        num_1 = some_list[i]
-        out_list = [num_1]
+        maxx = some_list[i]
+        out_list = [maxx]
         for j in range(i+1, len(some_list)):
-            if some_list[j] > num_1:
-                num_1 = some_list[j]
-                out_list.append(num_1)
+            if some_list[j] > maxx:
+                maxx = some_list[j]
+                out_list.append(maxx)
         if len(out_list) > 1:
             temp_list.append(out_list)
     return temp_list
 
-a = any_list(10)
-print(a)
-print(seq(a))
+n_list = any_list(int(input("Введите натуральное число: ")))
+print(n_list)
+new_list = seq(n_list)
+print(new_list)
 
