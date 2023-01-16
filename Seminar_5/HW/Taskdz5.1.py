0a279a83e8609f254a109b164bd4f4371e9c6d7e
BatmanovSergeydotnet <xoolly@mail.ru>
2023-01-16 20:27:59 +0300

    add correction_1 Taskdz5.1.

--- Seminar_5/HW/Taskdz5.1.py
+++ Seminar_5/HW/Taskdz5.1.py
@@ -20,22 +20,22 @@
 from random import sample
 
 def words_list (num, word="абв"):
-    if number > 0:
-        new_string = ""
-        new_string_short = ""
-        for i in range(num):
-            w = sample(word, k=3) 
-            m = ''.join (w)
-            new_string += m +" "
-            if m != "абв":
-                new_string_short += m +" "
-        return new_string, new_string_short
-    else:
-        return "Некорректное число"
+    new_string = ""
+    new_string_short = ""
+    for i in range(num):
+        w = sample(word, k=3) 
+        m = ''.join (w)
+        new_string += m +" "
+        if m != "абв":
+            new_string_short += m +" "
+    return new_string, new_string_short
 
 number = int(input('Введите число: '))
-out_string = words_list(number)
-print("Первоначальная строка: ")
-print(out_string[0])
-print("Строка с удалёнными 'абв': ")
-print(out_string[1])
+if number > 1:
+    out_string = words_list(number)
+    print("Первоначальная строка: ")
+    print(out_string[0])
+    print("Строка с удалёнными 'абв': ")
+    print(out_string[1])
+else:
+    print("Введено некорректное число")
